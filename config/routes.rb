e834722a395dad631219936eb472009042e13ff5
szymon niedorezo <szymonniedorezo@gmail.com>
2024-10-14 17:55:27 +0100

    added models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "recipes#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,4 +13,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :recipes, only: [:index, :show] do
+    resources :favourites, only: [:index, :create, :destroy, :edit, :update]
+  end
+  resources :ingredients, only: [:show] do
+    resources :ingredientreviews, only: [:create, :update, :destroy]
+  end
+
+
+
+
 end
