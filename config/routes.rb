74572391c6ed7bd219d9c97f9f0e70faeda21266
szymon niedorezo <szymonniedorezo@gmail.com>
2024-10-15 10:46:27 +0100

    reupload pull request

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "recipes#home"
+  root to: "recipes#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     resources :favourites, only: [:index, :create, :destroy, :edit, :update]
   end
   resources :ingredients, only: [:show] do
-    resources :ingredientreviews, only: [:create, :update, :destroy]
+    resources :ingredient_reviews, only: [:create, :update, :destroy]
   end
 
 
