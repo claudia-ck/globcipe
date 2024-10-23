f521f9249d275abee20e344ed3d7bce88a9a3bac
Claudia Kwok <ksnclaudia@gmail.com>
2024-10-23 10:34:01 +0100

    fix_the_map_zoom_rate

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,20 +11,23 @@ export default class extends Controller {
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
-    const map = new mapboxgl.Map({
+    // const map = new mapboxgl.Map({
+    this.map = new mapboxgl.Map({
       container: this.element,  // The container is still dynamically the current element
       style: "mapbox://styles/mapbox/streets-v12",
-      center: [-0.12,51.51], // starting position
-      zoom: 2 // starting zoom
+      // center: [-0.12,51.51], // starting position
+      // zoom: 2 // starting zoom
 
     });
 
     // Add markers to the map and fit to bounds
-    this.#addMarkersToMap(map);  // Pass the map instance as an argument
-    this.#fitMapToMarkers(map);  // Pass the map instance as an argument
+    this.#addMarkersToMap();  // Pass the map instance as an argument
+    this.#fitMapToMarkers();
+    // this.#addMarkersToMap(map);  // Pass the map instance as an argument
+    // this.#fitMapToMarkers(map); // Pass the map instance as an argument
   }
 
-  #addMarkersToMap(map) {
+  #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const customMarker = document.createElement('div')
       customMarker.style.height = "30px"
@@ -35,12 +38,12 @@ export default class extends Controller {
       new mapboxgl.Marker({element: customMarker})
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
-        // .addTo(this.map)
-        .addTo(map)
+        .addTo(this.map)
+        // .addTo(map)
     })
   }
 
-  #fitMapToMarkers(map) {
+  #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 100, maxZoom: 15, duration: 10 })
