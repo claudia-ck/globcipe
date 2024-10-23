7722bb3f155c810620eed28aabc79612a4967816
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-10-23 14:12:44 +0100

    restricted index page display as well as expanded grid

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,9 @@
 class RecipesController < ApplicationController
   def index
-    search = params[:search]
-    if search.present?
-      area = search[:area]
-      category = search[:category]
+    search = search_params
+    if search
+      area = search[:area] if search[:area] != ""
+      category = search[:category] if search[:category] != ""
       if area.present? && category.present?
         @recipes = Recipe.where(area: area, category: category)
       elsif area.present?
@@ -12,7 +12,7 @@ class RecipesController < ApplicationController
         @recipes = Recipe.where(category: category)
       end
     else
-      @recipes = Recipe.all
+      @recipes = category_driven_recipes
     end
   end
 
@@ -20,5 +20,27 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
     @favourite = Favourite.new
     # @ingredient = Ingredient.find(params[:ingredient_id])
+  end
+
+  private
+
+  def category_driven_recipes
+    categories = ['Beef', 'Breakfast', 'Chicken', 'Dessert', 'Goat', 'Lamb', 'Miscellaneous', 'Pasta', 'Pork',
+                  'Seafood', 'Side', 'Starter', 'Vegan', 'Vegetarian']
+    recipes_truncated = []
+    categories.each do |category|
+      recipes_category_specific = Recipe.where(category: category)
+      recipe_category_specific_random = recipes_category_specific[0]
+      recipes_truncated << recipe_category_specific_random
+    end
+    recipes_truncated << Recipe.last
+    recipes_truncated << Recipe.find_by(id: Recipe.last.id - 1)
+    return recipes_truncated
+  end
+
+  def search_params
+    return false if params[:search].nil?
+
+    params.require(:search).permit(:area, :category)
   end
 end
